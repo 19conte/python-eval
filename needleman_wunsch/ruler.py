ef6b229d28fad70c18a0ac1b8daa9ab0981e97bf
AlexC-11 <54978786+AlexC-11@users.noreply.github.com>
2019-12-23 14:42:11 +0100

    initalisation

---
+++ needleman_wunsch/ruler.py
@@ -0,0 +1,13 @@
+class Ruler(d = 1):
+
+    def __init__(self, brin1 : str, brin2 : str):
+        self.brin1 = brin1
+        self.brin2 = brin2
+
+    def compute(self):
+        ...
+        return self.distance
+    
+    def report(self):
+        ...
+        return brinf1, brinf2
